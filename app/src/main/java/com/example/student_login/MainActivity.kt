dc2739f5fd681c4645b0a56075e5f90c834c0426
qamar <qaishqamar333@gmail.com>
2019-10-18 09:51:24 +0530

    coaching dhundho login layout

---
+++ app/src/main/java/com/example/student_login/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.student_login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
